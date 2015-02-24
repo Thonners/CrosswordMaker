0c2fe12d93fb3f514d8224d687475e73fc20ce68
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2015-02-24 23:45:25 +0000

    Added: Dictionary Loading for anagram/wordfit TODO: Make it run in background

--- AnagramPageFragment.java
+++ AnagramPageFragment.java
@@ -12,12 +12,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 
 public class AnagramPageFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private static final String LOG_TAG = "AnagramPageFragment" ;
 
-    private static final String LOG_TAG = "AnagramPageFragment";
+    private int hashMapSize = 315000 ;   // Size for the hashMap to allow loadFactor < 0.75 whilst fiting all the words in the dictionary
+    private HashMap<String, ArrayList<String>> dictionaryHM = new HashMap<String, ArrayList<String>>(hashMapSize);    // Hashmap to store the options in
+
+    private ArrayList<String> dictionary = new ArrayList<String>() ;
+    private ArrayList<String[]> dictionaryByLetter= new ArrayList<String[]>() ;
+    private ArrayList<String> sortedDictionary = new ArrayList<String>() ;
+
+
+    private boolean buttonIsClear  = false ;    // Variable to store whether the button next to the search bar should be 'search' or 'clear'
+
+    private OnFragmentInteractionListener mListener;
 
     Button searchButton ;
     EditText inputBox ;
@@ -31,6 +45,13 @@ public class AnagramPageFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
         }
+
+        // Log how long it takes to load the dictionary
+        long startTime = System.currentTimeMillis() ;
+        Log.d(LOG_TAG,"AnagramPageFragment onCreate called @ millis: " + startTime);
+        loadDictionary();
+        long stopTime = System.currentTimeMillis();
+        Log.d(LOG_TAG,"Dictionary loaded after " + (stopTime - startTime) + " millis");
     }
 
     @Override
@@ -75,5 +96,79 @@ public class AnagramPageFragment extends Fragment {
     private void searchClicked() {
         // Do something
             Log.d(LOG_TAG, "Search button clicked. ");
+        toggleSearchButton();
+    }
+
+    private void toggleSearchButton() {
+        //Toggle the function of the search button
+        buttonIsClear = ! buttonIsClear ;
+        if (buttonIsClear) {
+            searchButton.setText(getString(R.string.clear));
+        } else {
+            searchButton.setText(getString(R.string.search));
+        }
+    }
+
+    private void loadDictionary() {
+        // Load the dictionaries in from the resouces
+
+        Log.d(LOG_TAG," Reading the dictionary...");
+
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsA));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsB));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsC));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsD));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsE));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsF));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsG));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsH));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsI));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsJ));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsK));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsL));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsM));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsN));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsO));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsP));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsQ));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsR));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsS));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsT));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsU));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsV));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsW));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsX));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsY));
+        dictionaryByLetter.add(getResources().getStringArray(R.array.wordsZ));
+        Log.d(LOG_TAG,"DictionaryByLetter Loaded...");
+        // Add all thr words to the dictionary
+        for (int i = 0 ; i < 26 ; i++) {
+        Log.d(LOG_TAG,"i=" + i);
+            for (int j =0 ; j < dictionaryByLetter.get(i).length ; j ++) {
+                String word = dictionaryByLetter.get(i)[j];
+                if (word != null) {
+                    dictionary.add(word);
+                    // Sort the string and add it to the ordered dictionary
+                    char[] wordChars = word.toLowerCase().toCharArray();
+                    Arrays.sort(wordChars);
+                    String sortedWord = new String(wordChars);
+                    sortedDictionary.add(sortedWord);
+
+
+                    // Add to the HashMap
+                    if (dictionaryHM.containsKey(sortedWord)) {
+                        ArrayList<String> possibleWords = dictionaryHM.get(sortedWord);
+                        possibleWords.add(word);
+                        dictionaryHM.put(sortedWord, possibleWords);
+                    } else {
+                        ArrayList<String> possibleWords = new ArrayList<String>();
+                        possibleWords.add(word);
+                        dictionaryHM.put(sortedWord, possibleWords);
+                    }
+                }
+            }
+        }
+
+        Log.d(LOG_TAG,"Dictionary & HashMap loaded...");
     }
 }
---
+++ AnagramSolver.java
@@ -0,0 +1,7 @@
+package com.thonners.crosswordmaker;
+
+/**
+ * Created by mat on 20/02/15.
+ */
+public class AnagramSolver {
+}
