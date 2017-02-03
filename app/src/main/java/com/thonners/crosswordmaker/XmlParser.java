42b7afbd6387d8043c9ab8381bb80d8a3314c8b0
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2017-02-03 23:45:26 +0000

    Add better Javadoc comments for DictionaryMWDownloadDefinition, XMLParser, and AnagramPageFragment.

--- app/src/main/java/com/thonners/crosswordmaker/XmlParser.java
+++ app/src/main/java/com/thonners/crosswordmaker/XmlParser.java
@@ -36,16 +36,31 @@ public class XmlParser {
     // We don't use namespaces
     private static final String ns = null;
 
+    /**
+     * Empty Constructor
+     */
     public XmlParser() {
-        // Empty Constructor
     }
 
+    /**
+     * Converts the input String to an InputStream, then calls {@link #parse(InputStream)}
+     * @param input The raw XML to be parsed
+     */
     public ArrayList<Entry> parse(String input) throws XmlPullParserException, IOException {
-    //    input = cleanTags(input);
         InputStream stream = new ByteArrayInputStream(input.getBytes(CHARSET));
         return parse(stream);
     }
 
+    /**
+     * Converts rawXML, as downloaded/received from the MerriamWebster dictionary and turns it into
+     * a list of {@link Entry}s.
+     *
+     * This method just sets up the {@link XmlPullParser}, then calls {@link #readFeed(XmlPullParser)}.
+     * @param in The InputStream of raw XML received from the MW dictionary.
+     * @return An ArrayList of {@link Entry}s, extracted from the input rawXML.
+     * @throws XmlPullParserException
+     * @throws IOException
+     */
     public ArrayList<Entry> parse(InputStream in) throws XmlPullParserException, IOException {
         try {
             XmlPullParser parser = Xml.newPullParser();
@@ -58,6 +73,19 @@ public class XmlParser {
         }
     }
 
+    /**
+     * Method to read the XML feed, and to turn it into a list of {@link Entry}s.
+     *
+     * Loops through all objects in the parser, ignoring the {@link XmlPullParser#START_TAG}, and
+     * finishing at the {@link XmlPullParser#END_TAG}.
+     *
+     * The useful results are read by {@link #readEntry(XmlPullParser)}. Other entries are {@link #skip(XmlPullParser)}'ed
+     *
+     * @param parser The parser instance containing the rawXML downloaded from MW.
+     * @return An ArrayList of {@link Entry}s, extracted from the input rawXML.
+     * @throws XmlPullParserException
+     * @throws IOException
+     */
     private ArrayList<Entry> readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
         ArrayList<Entry> entries = new ArrayList<>();
 
@@ -79,14 +107,25 @@ public class XmlParser {
         return entries;
     }
 
-
+    /**
+     * Wrapper method to read an 'entry', where the entry is the complete definition as received.
+     *
+     * For each type of XML tag read, this method then calls the appropriate method, from
+     * {@link #readWord(XmlPullParser)}, {@link #readWordType(XmlPullParser)}, or {@link #readDefinitions(XmlPullParser)},
+     * and adds the result to the appropriate part of the entry.
+     *
+     * @param parser The parser instance containing the rawXML downloaded from MW.
+     * @return An {@link Entry} containing the word, word type, definition, etc. as extracted from the XML.
+     * @throws XmlPullParserException
+     * @throws IOException
+     */
     private Entry readEntry(XmlPullParser parser)  throws XmlPullParserException, IOException {
         Log.d(LOG_TAG,"Entry found, reading entry...");
 
         parser.require(XmlPullParser.START_TAG, ns, XML_TAG_ENTRY);
         String word = null;
         String wordType = null;
-        ArrayList<String> definitions = new ArrayList<String>();
+        ArrayList<String> definitions = new ArrayList<>();
 
         while (parser.next() != XmlPullParser.END_TAG) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
@@ -107,6 +146,12 @@ public class XmlParser {
         return new Entry(word, wordType, definitions);
     }
 
+    /**
+     * @param parser The parser instance containing the rawXML downloaded from MW.
+     * @return The String representation of the word being defined.
+     * @throws IOException
+     * @throws XmlPullParserException
+     */
     private String readWord(XmlPullParser parser) throws IOException, XmlPullParserException {
         // Read the word in the word tag
         parser.require(XmlPullParser.START_TAG, ns, XML_TAG_WORD);
@@ -115,6 +160,13 @@ public class XmlParser {
         Log.d(LOG_TAG, "Word for entry found: " + word);
         return word;
     }
+
+    /**
+     * @param parser The parser instance containing the rawXML downloaded from MW.
+     * @return The type of word being defined, e.g. verb, adjective, noun, etc.
+     * @throws IOException
+     * @throws XmlPullParserException
+     */
     private String readWordType(XmlPullParser parser) throws IOException, XmlPullParserException {
         // Read the word type from the tag
         parser.require(XmlPullParser.START_TAG, ns, XML_TAG_WORD_TYPE);
@@ -123,6 +175,22 @@ public class XmlParser {
         Log.d(LOG_TAG,"WordType for entry found: " + wordType);
         return wordType ;
     }
+
+    /**
+     * Method to extract the definition(s) from the raw XML.
+     *
+     * For each separate definition, a new entry in the ArrayList is created.
+     *
+     * Complexity is added by the nested tags around certain words in a definition received from MW.
+     * e.g. Links to other words, etc. which are used on the website but not by this app.
+     * The method deals with these extra tags by keeping a count of how 'deep' the current XML object
+     * is, and then continuing to read data and add it to the current definition until the depth == 0.
+     *
+     * @param parser The parser instance containing the rawXML downloaded from MW.
+     * @return An ArrayList of String definitions for the word being defined.
+     * @throws IOException
+     * @throws XmlPullParserException
+     */
     private ArrayList<String> readDefinitions(XmlPullParser parser)  throws IOException, XmlPullParserException {
         // Extract all the definitions from the various numbered definitions
         ArrayList<String> definitions = new ArrayList<String>();
@@ -203,6 +271,14 @@ public class XmlParser {
 
         return definitions ;
     }
+
+    /**
+     * Method to actually pull the text out from between a set of XML tags.
+     * @param parser The parser instance containing the rawXML downloaded from MW.
+     * @return The String from between the tags
+     * @throws IOException
+     * @throws XmlPullParserException
+     */
     private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
         String result = "";
         if (parser.next() == XmlPullParser.TEXT) {
@@ -211,6 +287,13 @@ public class XmlParser {
         }
         return result;
     }
+
+    /**
+     * Method to skip to the next entry in the parser.
+     * @param parser The parser instance containing the rawXML downloaded from MW.
+     * @throws XmlPullParserException
+     * @throws IOException
+     */
     private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
         if (parser.getEventType() != XmlPullParser.START_TAG) {
             throw new IllegalStateException();
@@ -227,23 +310,45 @@ public class XmlParser {
             }
         }
     }
+
+    /**
+     * Class of dictionary entries, containing variables for the word being defined, the type of word,
+     * and the definition.
+     */
     public static class Entry {
         public String word ;
         public String wordType ;   // Noun, verb, etc.
-        public ArrayList<String> definitions = new ArrayList<String>();
+        public ArrayList<String> definitions = new ArrayList<>();
 
+        /**
+         * Constructor
+         * @param word The word defined by the definition
+         * @param wordType The type of word being defined.
+         * @param definitions An ArrayList of all the definitions for this word.
+         */
         public Entry(String word, String wordType, ArrayList<String> definitions){
             this.word = word ;
             this.wordType = wordType ;
             this.definitions = definitions;
         }
 
+        /**
+         * @return The word to which this entry refers.
+         */
         public String getWord() {
             return word ;
         }
+
+        /**
+         * @return The type of word being defined by this entry. E.g. verb, noun, adjective, etc.
+         */
         public String getWordType() {
             return wordType ;
         }
+
+        /**
+         * @return An ArrayList of all the definitions for this word.
+         */
         public ArrayList<String> getDefinitions() {
             return definitions ;
         }
