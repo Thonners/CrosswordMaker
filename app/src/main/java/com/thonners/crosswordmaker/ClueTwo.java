b7d27a10ae35f0dea7d00c216e33b75f29333101
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2024-04-23 18:18:48 +0100

    Define ClueTwo

--- app/src/main/java/com/thonners/crosswordmaker/ClueTwo.java
+++ app/src/main/java/com/thonners/crosswordmaker/ClueTwo.java
@@ -1,18 +1,58 @@
 package com.thonners.crosswordmaker;
 
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
 public class ClueTwo {
+
+    private final ArrayList<CellTwo> cells = new ArrayList<>();
 
     public enum Orientation {
         HORIZONTAL,
         VERTICAL
     }
 
-    private Orientation orientation;
+    private final Orientation orientation;
 
-    private CellTwo[] cells;
+    private final int clueNumber;
 
-    public ClueTwo(CellTwo[] cells, Orientation orientation) {
-        this.cells = cells;
+
+    public ClueTwo(Orientation orientation, int number) {
         this.orientation = orientation;
+        this.clueNumber = number;
+    }
+
+    public void addCell(CellTwo cell) {
+        cells.add(cell);
+    }
+
+    public ArrayList<CellTwo> getCells() {
+        return cells;
+    }
+
+    public CellTwo getFirstCell() {
+        if (!cells.isEmpty()) {
+            return cells.get(0);
+        }
+        return null;
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    public int getClueNumber() {
+        return clueNumber;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "ClueTwo{" +
+                "orientation=" + orientation +
+                ", clueNumber=" + clueNumber +
+                ", cells=" + cells +
+                '}';
     }
 }
